6658f7067724390e923423d333dda00f8e1c21fb
Robert Mayer <rwlmayer@gmail.com>
2015-09-07 15:11:10 +0200

    Added settings file.
    Settings file contains the necessary information to call the freshdesk
    api.

---
+++ IBSS.FreshDesk.Tests/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBSS.FreshDesk.Tests
+{
+    public static class Settings
+    {
+        public const string ApiKey = "YOUR_API-Key";
+        /// <summary>
+        /// The domain you are using, e.g. integrationsoftware
+        /// </summary>
+        public const string Domain = "YOUR-COMPANY-NAME";
+    }
+}
