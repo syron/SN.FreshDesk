f81e31d423668549a11563c5c3a3a99dabc0dba3
MarkMilfordMe <mark@milfords.eu>
2016-01-16 16:34:40 +0800

    Added support for Customers

---
+++ SN.FreshDesk.Tests/CustomerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using SN.FreshDesk.Models.Responses;
+using System.Threading.Tasks;
+using SN.FreshDesk.Models;
+using SN.FreshDesk.Models.Requests;
+
+namespace SN.FreshDesk.Tests
+{
+    [TestClass]
+    public class CustomerTests : BaseTestClass
+    {
+        [TestMethod]
+        public async Task GetCustomers()
+        {
+            List<customer> t = await fd.GetCustomers();
+        
+            Assert.IsNotNull(t);
+        }
+    }
+}
---
+++ SN.FreshDesk/FreshDesk_Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SN.FreshDesk.Models;
+
+namespace SN.FreshDesk
+{
+    public partial class FreshDesk
+    {
+        /// <summary>
+        /// Gets list of all customers
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<customer>> GetCustomers()
+        {
+            var relativeUrl = "/customers.json";
+            var x =  await SendGetRequest<SN.FreshDesk.Models.Responses.response_customer[]>(relativeUrl);
+            return  x.Select(f => f.customer).ToList();
+        }
+    }
+}
