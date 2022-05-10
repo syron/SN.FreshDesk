c7b013014ce1e452a0d721f0770281e1250f7477
Mark Milford <mark@markmilford.me>
2022-05-10 11:35:17 +0800

    Added blank class for missing item to get building

---
+++ SN.FreshDesk.Models/Responses/response_agents.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SN.FreshDesk.Models.Responses
+{
+    public class response_agents
+    {
+    }
+}
